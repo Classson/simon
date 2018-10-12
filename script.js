18929e6412d2cb3a0fd3ca2337232ce3376a6b32
Alex <Jillbk22@gmail.com>
2018-10-12 09:42:18 -0400

    audio and color flash for red button

--- script.js
+++ script.js
@@ -19,6 +19,21 @@ const randomColor = () => {
 //function that adds new random color to the array
 const addColor = () => gameArr.push(randomColor());
 
-let blueSnd = new Audio('http://www.pacdv.com/sounds/domestic_sound_effects/keys-catch-1.wav');
-snd.currentTime=0;
-blueSnd.play();
+
+
+function playRed(){
+   let redSnd = new Audio('http://www.pacdv.com/sounds/domestic_sound_effects/keys-catch-1.wav');
+
+    function sndRed(){
+        redSnd.currentTime=0;
+        redSnd.play();
+    }
+
+    function flashRed(){
+        document.getElementById('red').style.backgroundColor = "#ff4032";
+        setTimeout(function(){ document.getElementById('red').style.backgroundColor = "red"; }, 1000);
+    }
+    
+    sndRed();
+    flashRed();
+}
